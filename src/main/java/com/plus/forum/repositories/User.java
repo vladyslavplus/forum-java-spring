659394813ea144e8b3ccb668ce839904a1899dc1
vladyslavplus <vladislavperevispa12345@gmail.com>
2025-04-11 16:51:08 +0300

    Implements styles to auth.layout, add email field to User model

--- src/main/java/com/plus/forum/repositories/User.java
+++ src/main/java/com/plus/forum/repositories/User.java
@@ -19,6 +19,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    private String email;
     private String password;
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
