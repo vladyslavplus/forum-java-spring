33b9cdb04c8752335a85fd5d3be4631bb6c8728f
vladyslavplus <vladislavperevispa12345@gmail.com>
2025-04-17 09:03:00 +0300

    Add avatar, change styling for recent comments section

--- src/main/java/com/plus/forum/controllers/AuthController.java
+++ src/main/java/com/plus/forum/controllers/AuthController.java
@@ -1,28 +1,17 @@
 package com.plus.forum.controllers;
 
 import com.plus.forum.repositories.User;
-import com.plus.forum.repositories.UserRepository;
 import com.plus.forum.services.AuthService;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Set;
 
 @Controller
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
-    private final UserDetailsService userDetailsService;
     private final AuthService authService;
 
-    public AuthController(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, PasswordEncoder passwordEncoder, UserRepository userRepository, AuthService authService) {
-        this.authenticationManager = authenticationManager;
-        this.userDetailsService = userDetailsService;
+    public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
